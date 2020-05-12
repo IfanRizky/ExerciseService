cec697504e216d3a1a07005642cfbc8ff4fb5ca1
Mochammad Ifan Rizky Kurniawan <rizkykurniawan.ifan463@gmail.com>
2020-05-12 16:44:30 +0700

    add musicBinder class, setList method, onCompletion method, onError method, and onPrepared method

--- app/src/main/java/com/example/exerciseservice/MusicService.java
+++ app/src/main/java/com/example/exerciseservice/MusicService.java
@@ -4,6 +4,7 @@ import android.app.Service;
 import android.content.Intent;
 import android.media.AudioManager;
 import android.media.MediaPlayer;
+import android.os.Binder;
 import android.os.IBinder;
 import android.os.PowerManager;
 
@@ -14,43 +15,68 @@ public class MusicService extends Service implements
     MediaPlayer.OnErrorListener,
     MediaPlayer.OnCompletionListener {
 
-        //media player
-        private MediaPlayer player;
+    //media player
+    private MediaPlayer player;
 
-        //song list
-        private ArrayList<Song> songs;
+    //song list
+    private ArrayList<Song> songs;
 
-        //current position
-        private int songPosn;
+    //current position
+    private int songPosn;
 
-        @Override
-        public IBinder onBind(Intent intent) {
-            // TODO Auto-generated method stub
-            return null;
+    @Override
+    public IBinder onBind(Intent intent) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    public void onCreate() {
+        //create the service
+        super.onCreate();
+
+        //initialize position
+        songPosn = 0;
+
+        //create player
+        player = new MediaPlayer();
+
+        initMusicPlayer();
+    }
+
+    public void initMusicPlayer(){
+        //set player properties
+        player.setWakeMode(getApplicationContext(),
+                PowerManager.PARTIAL_WAKE_LOCK);
+        player.setAudioStreamType(AudioManager.STREAM_MUSIC);
+
+        player.setOnPreparedListener(this);
+        player.setOnCompletionListener(this);
+        player.setOnErrorListener(this);
+    }
+
+    public void setList(ArrayList<Song> theSongs){
+        songs = theSongs;
+    }
+
+    public class MusicBinder extends Binder {
+        MusicService getService() {
+            return MusicService.this;
         }
+    }
 
-        public void onCreate() {
-            //create the service
-            super.onCreate();
+    @Override
+    public void onCompletion(MediaPlayer mp) {
 
-            //initialize position
-            songPosn = 0;
+    }
 
-            //create player
-            player = new MediaPlayer();
+    @Override
+    public boolean onError(MediaPlayer mp, int what, int extra) {
+        return false;
+    }
 
-            initMusicPlayer();
-        }
+    @Override
+    public void onPrepared(MediaPlayer mp) {
 
-        public void initMusicPlayer(){
-            //set player properties
-            player.setWakeMode(getApplicationContext(),
-                    PowerManager.PARTIAL_WAKE_LOCK);
-            player.setAudioStreamType(AudioManager.STREAM_MUSIC);
-
-            player.setOnPreparedListener(this);
-            player.setOnCompletionListener(this);
-            player.setOnErrorListener(this);
-        }
+    }
 }
 
