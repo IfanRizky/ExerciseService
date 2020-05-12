74a2ecd59a546dcc9517c80dcc0b8990cf11c197
Mochammad Ifan Rizky Kurniawan <rizkykurniawan.ifan463@gmail.com>
2020-05-12 15:18:09 +0700

    add new variables in MainActivity class

--- app/src/main/java/com/example/exerciseservice/MainActivity.java
+++ app/src/main/java/com/example/exerciseservice/MainActivity.java
@@ -3,8 +3,14 @@ package com.example.exerciseservice;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ArrayList<Song> songList;
+    private ListView songView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
